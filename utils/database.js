bf1b185814c629e46a47b12268772775504a95dd
cyll99 <christyanlove@gmail.com>
2024-11-05 10:03:56 -0500

    [CHANGE] database location

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-  host: "localhost",
-  username: "postgres",
-  database: "portfolio",
-  password: "Jesus123!!",
-  port: 5433,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // for most hosted databases like Railway
+      rejectUnauthorized: false, // disables SSL certificate verification
+    },
+  },
 });
 
 export default sequelize;
